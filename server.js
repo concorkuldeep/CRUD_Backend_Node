9bdc47d227c10537bbcb9a421d12b3ef8116984e
concormobapp <concormobapp@gmail.com>
2026-01-20 11:11:17 +0530

    basic changes done

--- server.js
+++ server.js
@@ -10,14 +10,11 @@ const server = app.listen(PORT,()=>{
     console.log(`Server is running on ${process.env.NODE_ENV} mode on port ${PORT}`)
 })
 
-// Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`);
-    // Close server & exit process
     server.close(() => process.exit(1));
   });
   
-  // Handle uncaught exceptions
   process.on('uncaughtException', (err) => {
     console.log(`Error: ${err.message}`);
     process.exit(1);
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,38 +3,38 @@ const User = require('../models/UserModel');
 
 
 
-const protect = async(req,res,next) =>{
-let token;
-if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-    token = req.headers.authorization.split(' ')[1]
-}
+const protect = async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1]
+    }
 
-if(!token){
-    return res.status(401).json({
-        success:false,
-        message:'Unauthorized, Token Expired.'
-    })
-}
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: 'Unauthorized, Token Expired.'
+        })
+    }
 
-try{
-const decoded = jwt.verify(token,process.env.JWT_SECRET);
-req.user = await User.findById(decoded.id);
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id);
 
-if(!req.user){
-    return res.status(200).json({
-        success:false,
-        message:'User Not Found'
-    })
-}
+        if (!req.user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User Not Found'
+            })
+        }
 
-next();
+        next();
 
-}catch(e){
-return res.status(401).json({
-    success:false,
-    message:'Unauthorized'
-})
-}
+    } catch (e) {
+        return res.status(401).json({
+            success: false,
+            message: 'Unauthorized'
+        })
+    }
 
 
 
